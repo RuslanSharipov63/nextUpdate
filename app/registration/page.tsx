d92bcf7f1716218c83226a1659712222196baf7f
unknown <sharipov.r@mail.ru>
2023-07-14 00:29:29 +0400

    create account page

--- app/registration/page.tsx
+++ app/registration/page.tsx
@@ -1,13 +1,17 @@
-"use client"
+"use client";
 import { useState } from "react";
-import { validationEmail, validationPassword, validationFirstName } from "@/helper/validation";
+import {
+  validationEmail,
+  validationPassword,
+  validationFirstName,
+} from "@/helper/validation";
 import TextField from "@/components/TextField";
-
+import Button from "@/components/Button";
+import styles from "./registration.module.css";
 
 type stateProps = {
   [x: string]: string;
-}
-
+};
 
 const RegistrationPage = () => {
   const [registerInput, setRegisterInput] = useState<stateProps>({
@@ -38,65 +42,95 @@ const RegistrationPage = () => {
   };
 
   const handleBlur = (etn: string): void => {
-    if (etn === 'email') {
-      if (validationEmail(registerInput.etn) === false) {
+    if (etn === "email") {
+      if (validationEmail(registerInput.email) === false) {
         setError({ ...error, [etn]: "Не валидный email" });
       }
     }
-
-    if (etn === 'password') {
-      if (validationPassword(registerInput.etn) === false) {
+    if (etn === "password") {
+      if (validationPassword(registerInput.password) === false) {
         setError({ ...error, [etn]: "Пароль от 5 до 8 символов" });
       }
     }
 
-    if (etn === 'firstName') {
-      if (validationFirstName(registerInput.etn) === false) {
+    if (etn === "firstName") {
+      if (validationFirstName(registerInput.firstName) === false) {
         setError({ ...error, [etn]: "Имя от 3 до 10 символов" });
       }
     }
-
   };
+
+  const checkAuth = (e: any) => {
+    e.preventDefault();
+    if (
+      validationPassword(registerInput.password) === false ||
+      validationEmail(registerInput.email) === false
+    ) {
+      setCheckEmailPass(false);
+    } else {
+      alert("Вход разрешен");
+    }
+  };
+
   return (
-    <>
-      <p>
-        <label htmlFor="firstname">Имя</label>
-      </p>
-      <TextField
-        typeText={"text"}
-        valueText={registerInput.firstName}
-        funcChange={handleChange}
-        funcFocus={handleFocus}
-        nameText="firstName"
-        idText={"firstname"}
-        funcBlur={handleBlur}
-      />
-      <span className="helper-text">{error.firstName === "" ? null : error.firstName}</span>
-      <label htmlFor="email">Электронная почта</label>
-      <TextField
-        typeText={"text"}
-        valueText={registerInput.email}
-        funcChange={handleChange}
-        funcFocus={handleFocus}
-        nameText="email"
-        idText={"email"}
-        funcBlur={handleBlur}
-      />
-      <span className="helper-text">{error.email === "" ? null : error.email}</span>
-      <p>
-        <label htmlFor="password">Пароль</label>
-      </p>
-      <TextField
-        typeText={"password"}
-        valueText={registerInput.password}
-        funcChange={handleChange}
-        funcFocus={handleFocus}
-        nameText="password"
-        idText={"pass"}
-        funcBlur={handleBlur}
-      />
-      <span className="helper-text">{error.password === "" ? null : error.password}</span>
-    </>
+    <div className={styles.container} style={{ marginBottom: "200px" }}>
+      <div></div>
+      <form>
+        <div>
+          <p>
+            <label htmlFor="firstname">Имя</label>
+          </p>
+          <TextField
+            typeText={"text"}
+            valueText={registerInput.firstName}
+            funcChange={handleChange}
+            funcFocus={handleFocus}
+            nameText="firstName"
+            idText={"firstname"}
+            funcBlur={handleBlur}
+          />
+          <p>
+            <span className="helper-text">
+              {error.firstName === "" ? null : error.firstName}
+            </span>
+          </p>
+          <label htmlFor="email">Электронная почта</label>
+          <TextField
+            typeText={"text"}
+            valueText={registerInput.email}
+            funcChange={handleChange}
+            funcFocus={handleFocus}
+            nameText="email"
+            idText={"email"}
+            funcBlur={handleBlur}
+          />
+          <p>
+            <span className="helper-text">
+              {error.email === "" ? null : error.email}
+            </span>
+          </p>
+          <p>
+            <label htmlFor="password">Пароль</label>
+          </p>
+          <TextField
+            typeText={"password"}
+            valueText={registerInput.password}
+            funcChange={handleChange}
+            funcFocus={handleFocus}
+            nameText="password"
+            idText={"pass"}
+            funcBlur={handleBlur}
+          />
+          <p>
+            <span className="helper-text">
+              {error.password === "" ? null : error.password}
+            </span>
+          </p>
+        </div>
+        <Button text={"войти"} funcClick={checkAuth} />
+      </form>
+      <div></div>
+    </div>
   );
 };
 
