6c6ef6873958a78860e3395deabe0bd64fd38b2f
unknown <sharipov.r@mail.ru>
2023-07-20 00:22:33 +0400

    create slice

--- components/PhotoList.tsx
+++ components/PhotoList.tsx
@@ -3,80 +3,9 @@ import style from "./../stylescomponent/PhotoList.module.css";
 import Button from "./Button";
 
 const PhotoList = () => {
-<<<<<<< HEAD
-    return (
-        <>
-
-            <div className={style.container}>
-                <div className="card">
-                    <div className="card-image">
-                        <Image
-                            src="/image/photo/s1200.jpg"
-                            width={200}
-                            height={300}
-                            alt="картинка"
-                            priority={true}
-                            style={{ objectFit: "cover" }}
-                        />
-                        <span className="card-title">Card Title</span>
-                    </div>
-                    <div className="card-content">
-                        <p>I am a very simple card. I am good at containing small bits of information.
-                            I am convenient because I require little markup to use effectively.</p>
-                    </div>
-                    <div className="card-action">
-                        <a href="#">This is a link</a>
-                    </div>
-                </div>
-                <div className="card">
-                    <div className="card-image">
-                        <Image
-                            src="/image/photo/s1200.jpg"
-                            width={200}
-                            height={300}
-                            alt="картинка"
-                            priority={true}
-                            style={{ objectFit: "cover" }}
-                        />
-                        <span className="card-title">Card Title</span>
-                    </div>
-                    <div className="card-content">
-                        <p>I am a very simple card. I am good at containing small bits of information.
-                            I am convenient because I require little markup to use effectively.</p>
-                    </div>
-                    <div className="card-action">
-                        <a href="#">This is a link</a>
-                    </div>
-                </div>
-                <div className="card">
-                    <div className="card-image">
-                        <Image
-                            src="/image/photo/s1200.jpg"
-                            width={200}
-                            height={300}
-                            alt="картинка"
-                            priority={true}
-                            style={{ objectFit: "cover" }}
-                        />
-                        <span className="card-title">Card Title</span>
-                    </div>
-                    <div className="card-content">
-                        <p>I am a very simple card. I am good at containing small bits of information.
-                            I am convenient because I require little markup to use effectively.</p>
-                    </div>
-                    <div className="card-action">
-                        <a href="#">This is a link</a>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
-}
-=======
   const funcDeletePhoto = () => {
     alert("Фото удалено");
   };
->>>>>>> 726d07ee7bfba54079cc46bf8213621780d47766
 
   return (
     <>
--- store/PhotoListSlice.ts
+++ store/PhotoListSlice.ts
@@ -1,25 +1,52 @@
-/* import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 
- const PhotoList = createAsyncThunk(
-    "photo/getAllPhoto",
-    async function (_, thunkAPI) {
-        const response = await fetch('asdfdsaf')
-        return response.data;
-    }
-)  
+const PhotoListAsyncThunk = createAsyncThunk(
+  "photo/getAllPhoto",
+  async function (_, thunkAPI) {
+    const response = await fetch("http://localhost:4000/photos");
+    return response.json();
+  }
+);
 
-type initialStateType = {
-    imageURL: string;
-    tags: Array<string>;
-    user: string,
-    size: number;
-
+interface IinitialStateList {
+  imageURL: string;
+  tags: Array<string>;
+  user: string;
+  size: number;
 }
 
-const initialState: initialStateType[] = [];
+type initialStateType = {
+  list: IinitialStateList[];
+  loading: "idle" | "pending" | "succeeded" | "failed";
+};
 
-export const PhotoList = createSlice({
-    name: 'photolist',
-    initialState, 
-    reducers: 
-}) */
+const initialState: initialStateType = {
+  list: [
+    {
+      imageURL: "",
+      tags: [],
+      user: "",
+      size: 0,
+    },
+  ],
+  loading: "idle",
+};
+
+export const PhotoListSlice = createSlice({
+  name: "photolist",
+  initialState,
+  reducers: {
+    getPhotoList: (state, action: PayloadAction<IinitialStateList[]>) => {
+      state.list = action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(PhotoListAsyncThunk.fulfilled, (state, action) => {
+      state.list = action.payload;
+      state.loading = "succeeded";
+    });
+  },
+});
+
+export const { getPhotoList } = PhotoListSlice.actions;
+export default PhotoListSlice.reducer;
